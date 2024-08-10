ee41b5e240b0492b569ffefc5e080a315c663617
Diego Carrasco <diegocarrasco.ordonez@gmail.com>
2024-08-10 15:27:53 -0400

    cambios varios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from "react"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Register from './components/Register'
 import Home from './components/Home'
+import './App.css'
+import Login from "./components/Login"
+
+
+
 
 const App = () => {
   return (
-    <div className="container-fluid p-0">
+    <div className="app-container">
       <Navbar />
-      <Home />
+      <main className="main-content">
+      <Login/> 
+      </main>     
       <Footer />
     </div>
   )
