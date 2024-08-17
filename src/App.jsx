d095d23ced92e2d387f6128eba7d255b908e74f9
Diego Carrasco <diegocarrasco.ordonez@gmail.com>
2024-08-16 23:26:21 -0400

    cambios y pruebas varias

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Home from './components/Home'
 import './App.css'
 import Login from "./components/Login"
 import Cart from "./components/Cart"
-// import pizzaCart from "./pizzaCart"
 import { pizzaCart } from "./pizzas"
 import { useState } from "react"
 
@@ -14,20 +13,14 @@ import { useState } from "react"
 const App = () => {
     const [cart, setCart] = useState(pizzaCart)
 
-    // Funcion para calcular la cantidad total de productos
     const totalQuantity = cart.reduce((acc, pizza) => acc + pizza.quantity, 0)
-
-    // Funcion para calcular el total del carrito
     const totalPrice = cart.reduce((acc, pizza) => acc + pizza.price * pizza.quantity, 0)
 
   return (
-    // <div className="container-fluid p-0">
     <div className="app-container">
       <Navbar totalQuantity={totalQuantity} totalPrice={totalPrice}/>
       <main className="main-content">
       <Home />
-      {/* <Register/>
-      <Login/>  */}
       </main>   
       <Cart cart={cart} setCart={setCart}/>  
       <Footer />
